229ca29088dfa7e2d2c46f2d4143a9a92dbec69a
Patrick Sørensen <hasfeldt@yahoo.dk>
2023-12-27 17:01:57 +0100

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -8,10 +8,10 @@ TA:
     - Port: The port of the TA server
     - Password: The password of the TA server, leave blank if there is no password
 
-Gamemode: VERSUS / BR / PokéSaber
+Gamemode: VERSUS / BR / PS
     - VERSUS is the normal 1v1, 2v2 version, which is mostly controlled by the webpanel (Pick's'Ban's, Add user to overlay + show current map, etc.)
     - BR is the Battle Royale version. Current map is controlled by the server directly. Webpanel is used for picking who's being spectated, dead/alive/score 
-    - PokéSaber is the Pokémon style tournament. Nothing on the frontend is controlled by the relay-server, only sending scores to the frontend.
+    - PS, aka PokéSaber, is the Pokémon style tournament. Nothing on the frontend is controlled by the relay-server, only sending scores to the frontend.
 */
 export default {
     Server: {
